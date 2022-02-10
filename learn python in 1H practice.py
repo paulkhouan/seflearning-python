14d974a79871d93de26943aec936ae71c55ebfd3
paulkhouan <99380502+paulkhouan@users.noreply.github.com>
2022-02-10 11:39:43 +0700

    Add files via upload

---
+++ learn python in 1H practice.py
@@ -0,0 +1,125 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# exercice 1
+# declare variable for John Smith, 20, new patient
+# name = 'john Smith'
+# age = 20
+# status = 'new patient'
+
+# name = input('What is your name? ')
+# print('hello ' + name)
+
+# birth_year = input('enter your birth year: ')
+# age = 2020 - int(birth_year)
+# print(age)
+
+# First_num = input('First: ')
+# Second_num = input('Second: ')
+# Sum = float(First_num) + int(Second_num)
+# print('Sum: ' + Sum) Return error: cannot concatenate string + float
+
+# solution: turn Sum into str()
+# First_num = input('First: ')
+# Second_num = input('Second: ')
+# Sum = float(First_num) + int(Second_num)
+
+# Alt solution
+# First_num = float(input('First: '))
+# Second_num = float(input('Second: '))
+# Sum = First_num + Second_num
+# print('Sum: ' + str(Sum))
+
+# strings 18:53
+# course = 'Python for beginners'
+#  print(course)
+# print('Python' in course)
+
+# 33:40 if statement
+# temperature = 11
+
+# if temperature > 30:
+#    print("it's a hot day")
+#    print('drink plenty of water')
+# elif temperature > 20:
+#    print("It's a nice day")
+# elif temperature > 10:
+#    print("It's a bit cold")
+# else:
+#    print("It's cold")
+# print("Done")
+
+# weight = float(input('Weight: '))
+# unit = input('(k)g or (L)bs: ')
+
+# if unit.upper() == "K":
+#    converted = weight / 0.45
+#    print("Weight in Lbs: " + str(converted))
+# else:
+#    converted = weight * 0.45
+#    print("Weight in Kgs: " + str(converted))
+# print("done.")
+
+#07 fev 2022
+# while
+# i = 1
+#while i <= 1_00:
+    #print(i)
+#    print(i * '*')
+#    i = i + 1
+
+# Lists
+
+# names = ["john", "margaret", "Lucie", "Andy" ]
+# print(names)
+#>>>['john', 'margaret', 'Lucie', 'Andy']
+# print(names[0])
+#>>> john
+# print(names[-1])
+#>>> Andy
+# print(names[-2])
+#>>>Lucie
+# names[0] = "Jon"
+# print(names[0:2])
+# >>>['Jon', 'margaret']
+# print(names[-1:-2])
+#>>>[] Les nombre negatifs ne donnent rien
+
+# list methods
+# numbers = [1, 2, 3, 4, 5, 6]
+# numbers.append(8)
+# numbers.insert(0, -1)
+# numbers.remove(3)
+# print(1 in numbers)
+# print(len(numbers))
+
+# For loops 52:18
+numbers = [1, 2, 3, 4, 5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
